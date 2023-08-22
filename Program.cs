f6d644b9a677e1602f20bacfe25633fed4695765
James Henry <jhenry@vigosoftware.com>
2023-08-22 16:10:09 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var escapePressed = false;
+while (!escapePressed)
+{
+    var pos = Console.GetCursorPosition();
+    var pressedKey = Console.ReadKey();
+    if (pressedKey.Key == ConsoleKey.Escape)
+    {
+        escapePressed = true;
+    }
+    switch (pressedKey.Key)
+    {
+        case ConsoleKey.H:
+            Console.SetCursorPosition(pos.Left - 1, pos.Top);
+            break;
+        case ConsoleKey.J:
+            Console.SetCursorPosition(pos.Left, pos.Top + 1);
+            break;
+        case ConsoleKey.K:
+            Console.SetCursorPosition(pos.Left, pos.Top - 1);
+            break;
+        case ConsoleKey.L:
+            Console.SetCursorPosition(pos.Left + 1, pos.Top);
+            break;
+        default:
+            break;
+    }
+}
